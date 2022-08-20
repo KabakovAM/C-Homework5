5bf3a60749b7b700cee63975f44c72b7809430a3
AntonK <ak@m.ru>
2022-08-20 11:11:23 +0300

    Задание выполнено

---
+++ Ex034/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateFillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2==0) count++;
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}]");
+    }
+}
+
+int[] arr = CreateFillArray(10, 100, 999);
+int result = CountEvenNumbers(arr);
+Console.Write("В массиве ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+PrintArray(arr);
+Console.ResetColor();
+Console.Write(" количество чётных чисел равно ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write(result);
+Console.ResetColor();
---
+++ Ex038/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементами массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateFillArrayDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Math.Round(rnd.NextDouble() * (max - min) + min, 1, MidpointRounding.ToZero);
+    }
+    return array;
+}
+
+double CompareMinMax(double[] array)
+{
+    double result = 0;
+    int indexMin = 0;
+    int indexMax = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[indexMin] > array[i]) indexMin = i;
+        if (array[indexMax] < array[i]) indexMax = i;
+    }
+    result = array[indexMax] - array[indexMin];
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}]");
+    }
+}
+
+double[] arr = CreateFillArrayDouble(10, -100, 100);
+double result = CompareMinMax(arr);
+Console.WriteLine("Разница между максимальным и минимальным элементом массива ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+PrintArray(arr);
+Console.ResetColor();
+Console.Write(" равна ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write(result);
+Console.ResetColor();
